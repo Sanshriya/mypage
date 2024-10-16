d93a090c8e7bb98873e0b98ddda2e094ee71ce69
Sana Chikkam <schik002@gold.ac.uk>
2024-10-16 12:37:36 +0100

    Added a smiley face

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var http = require("http"); 
+var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/html"});
-    res.end("<title>My Page</title>"+"<h1>About Me</h1>"+"<h3>I am hungry</h3>"+"<p>I want to eat rice</p>"); // page content
+    res.end("<title>My Page</title>"+"<h1>About Me</h1>"+"<h3>I am hungry</h3>"+"<p>I want to eat rice :)</p>"); // page content
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 });
